388d0df657523bb240ee94ece50cb4fbcbf2c9ac
jorgeluis21 <jcarabalica@gmail.com>
2019-09-16 18:49:07 -0500

    Create blanket_patterns.rb
    A little game to generate a pattern given some string input from the user.

---
+++ blanket_patterns.rb
@@ -0,0 +1,14 @@
+#Blanket patterns by Jorge Carabali
+
+
+#Pattern given by user, you might introduce any pattern that you want.
+user_pattern = "= | / | / | = | / | = &  # / | - = | / \ | = \ & #"
+user_pattern_array= user_pattern.split(" ")
+
+for item in user_pattern_array   #iterates over the array 
+    user_pattern_array.push(user_pattern_array[0])  #sends first element of the array to the last position of the array
+    user_pattern_array.shift  #delete the first element of the array
+    puts "#{user_pattern_array.join(" ")}"   #converts to a single string.
+
+end   
+
